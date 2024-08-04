6067df3ecfce56fc0cd26543d7260d6cc0ef9fb1
arianaxgalindo <143916855+arianaxgalindo@users.noreply.github.com>
2024-08-03 20:18:07 -0400

    homepage - 2:43

--- full-stack/vite-project/src/App.tsx
+++ full-stack/vite-project/src/App.tsx
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-import './globals.css';
 import SigninForm from './_auth/forms/SinginForm';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Home } from './_root/pages';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster";
+import Explore from './_root/pages/Explore';
+import AllUsers from './_root/pages/AllUsers';
+import Saved from './_root/pages/Saved';
+import CreatePost from './_root/pages/CreatePost';
+import EditPost from './_root/pages/EditPost';
+import PostDetails from './_root/pages/PostDetails';
+import Profile from './_root/pages/Profile';
+import UpdateProfile from './_root/pages/UpdateProfile';
+
 
 const App = () => {
     return (
@@ -19,9 +29,19 @@ const App = () => {
                 {/* private routes */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path='/explore' element={<Explore />} />
+                    <Route path='/saved' element={<Saved />} />
+                    <Route path='/all-users' element={<AllUsers />} />
+                    <Route path='/create-post' element={<CreatePost  />} />
+                    <Route path='/update-post/:id' element={<EditPost />} />
+                    <Route path='/posts/:id' element={<PostDetails />} />
+                    <Route path='/profile/:id/' element={<Profile />} />
+                    <Route path='/update-profile/:id' element={<UpdateProfile />} />
                 </Route>
                 
             </Routes>
+ 
+            <Toaster />
         </main>
     )
 }
--- full-stack/vite-project/src/_auth/forms/SignupForm.tsx
+++ full-stack/vite-project/src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,10 @@
 // import React from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-import { Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form"
+import { useToast } from "@/components/ui/use-toast"
+import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
 import { Button } from "@/components/ui/button"
@@ -11,10 +12,17 @@ import { useForm } from "react-hook-form"
 import { SignupValidation } from "@/lib/validation"
 import { z } from "zod"
 import { Loader } from "lucide-react"
-import { createUserAccount } from "@/lib/appwrite/api"
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
+import { useUserContext } from "@/context/AuthContext"
 
 const SignupForm = () => {
-    const isLoading = false;
+    const { toast } = useToast();
+    const { checkAuthUser, isLoading: isUserLoading} = useUserContext();
+    const navigate = useNavigate();
+
+    const {mutateAsync: createUserAccount, isPending: isCreatingAccount} = useCreateUserAccount(); 
+
+    const {mutateAsync: singInAccount, isPending: isSigningIn} = useSignInAccount();
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof SignupValidation>>({
@@ -30,7 +38,33 @@ const SignupForm = () => {
     // 2. Define a submit handler.
     async function onSubmit(values: z.infer<typeof SignupValidation>) {
         const newUser = await createUserAccount(values);
-        console.log(newUser) 
+        
+        if(!newUser){
+            return toast({
+                title: 'Sign up failed. Please try again.'
+            });
+        }
+
+        const session = await singInAccount({
+            email: values.email, password: values.password,
+        })
+
+        if(!session) {
+            toast({title: 'Sign in failed. Please try again'});
+
+            navigate("/sign-in");
+            return;
+        }
+
+        const isLoggedIn = await checkAuthUser();
+
+        if(isLoggedIn){
+            form.reset();
+
+            navigate('/')
+        } else{
+            return toast({title: 'sign up failed.Please try again.'})
+        }
     }
 
     return(
@@ -94,7 +128,7 @@ const SignupForm = () => {
                         )}
                         />
                         <Button type="submit" className="shad-button_primary">
-                            {isLoading ? (
+                            {isCreatingAccount ? (
                                 <div className="flex-center gap-2">
                                    <Loader /> Loading...
                                 </div>
